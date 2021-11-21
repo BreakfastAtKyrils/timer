a882311640a482067ff035b29393e4c6839d6be4
BreakfastAtKyrils <kyrildr@gmail.com>
2021-11-21 09:57:47 -0500

    timer1 refactoring

--- timer1.js
+++ timer1.js
@@ -1,10 +1,27 @@
 
 
-process.argv.forEach(function (value) {
-  
+const timer = process.argv.forEach(function (value) {
+
+  //if there is no user input
+  if (value === null){
+    return null;
+  }
+
+  //if the input > 0, and if the input is a Number
   if (value > 0 && Number.isInteger(parseInt(value))) {
     setTimeout(() => {
       process.stdout.write('\x07');
+      console.log(value)
     }, value * 1000) 
   }
 });
+
+//modules.exports = timer;
+
+//feedback
+/*
+  values in a variable (input)
+  start look after first 2 elements to avoid extra computation
+  more functions
+
+*/
